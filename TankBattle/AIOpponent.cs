b33ab0dd89e2411958e85440a0f7f6d44bb7208a
AsianJohnBoi <32500331+AsianJohnBoi@users.noreply.github.com>
2017-10-04 10:00:34 +1000

    Add files via upload

---
+++ TankBattle/AIOpponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankBattle
+{
+    public class AIOpponent : Opponent
+    {
+        public AIOpponent(string name, TankModel tank, Color colour) : base(name, tank, colour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartRound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void BeginTurn(GameplayForm gameplayForm, Gameplay currentGame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ProjectileHitPos(float x, float y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TankBattle/Gameplay.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TankBattle
+{
+    public class Gameplay
+    {
+        public Gameplay(int numPlayers, int numRounds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PlayerCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalRounds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPlayer(int playerNum, Opponent player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Opponent GetPlayerNumber(int playerNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ControlledTank PlayerTank(int playerNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Color GetTankColour(int playerNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int[] CalculatePlayerPositions(int numPlayers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Shuffle(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeginRound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Terrain GetLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawPlayers(Graphics graphics, Size displaySize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ControlledTank GetCurrentGameplayTank()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddWeaponEffect(WeaponEffect weaponEffect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProcessEffects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RenderEffects(Graphics graphics, Size displaySize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndEffect(WeaponEffect weaponEffect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckHitTank(float projectileX, float projectileY)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InflictDamage(float damageX, float damageY, float explosionDamage, float radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Gravity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TurnOver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RewardWinner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NextRound()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int GetWindSpeed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
