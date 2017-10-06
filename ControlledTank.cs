18ea4745e1da65144c78fd0d384eebff21d971bd
AsianJohnBoi <32500331+AsianJohnBoi@users.noreply.github.com>
2017-10-06 16:42:49 +1000

    Add files via upload

---
+++ ControlledTank.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankBattle
+{
+    public class ControlledTank
+    {
+        private Opponent player;
+        private int tankX;
+        private int tankY;
+        private TankModel tankModel;
+        private int currentDur;
+
+        public ControlledTank(Opponent player, int tankX, int tankY, Gameplay game)
+        {
+            this.player = player;
+            this.tankX = tankX;
+            this.tankY = tankY;
+            tankModel = player.GetTank();
+            currentDur = tankModel.GetArmour();
+        }
+
+        public Opponent GetPlayerNumber()
+        {
+            return player;
+        }
+        public TankModel GetTank()
+        {
+            return tankModel;
+        }
+
+        private int angle;
+        public float GetAim()
+        {
+            return angle; 
+        }
+
+        public void SetAimingAngle(float angle)
+        {
+            if (angle >= -90 && angle <= 180){ this.angle = (int)angle; }
+        }
+
+        private int power;
+        public int GetCurrentPower()
+        {
+            return power;
+        }
+
+        public void SetPower(int power)
+        {
+            this.power = power;
+        }
+
+        public int GetWeaponIndex()
+        {
+            throw new NotImplementedException();
+        }
+        public void SetWeaponIndex(int newWeapon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(Graphics graphics, Size displaySize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetX()
+        {
+            throw new NotImplementedException();
+        }
+        public int GetYPos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InflictDamage(int damageAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists()
+        {
+            if (currentDur > 0) { return true; }
+            else { return false; }
+        }
+
+        public bool Gravity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TankModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankBattle
+{
+    public abstract class TankModel
+    {
+        public const int WIDTH = 4;
+        public const int HEIGHT = 3;
+        public const int NUM_TANKS = 1;
+
+        public abstract int[,] DisplayTank(float angle);
+
+        public static void DrawLine(int[,] graphic, int X1, int Y1, int X2, int Y2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bitmap CreateBitmap(Color tankColour, float angle)
+        {
+            int[,] tankGraphic = DisplayTank(angle);
+            int height = tankGraphic.GetLength(0);
+            int width = tankGraphic.GetLength(1);
+
+            Bitmap bmp = new Bitmap(width, height);
+            Color transparent = Color.FromArgb(0, 0, 0, 0);
+            Color tankOutline = Color.FromArgb(255, 0, 0, 0);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (tankGraphic[y, x] == 0)
+                    {
+                        bmp.SetPixel(x, y, transparent);
+                    }
+                    else
+                    {
+                        bmp.SetPixel(x, y, tankColour);
+                    }
+                }
+            }
+
+            // Outline each pixel
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                    if (tankGraphic[y, x] != 0)
+                    {
+                        if (tankGraphic[y - 1, x] == 0)
+                            bmp.SetPixel(x, y - 1, tankOutline);
+                        if (tankGraphic[y + 1, x] == 0)
+                            bmp.SetPixel(x, y + 1, tankOutline);
+                        if (tankGraphic[y, x - 1] == 0)
+                            bmp.SetPixel(x - 1, y, tankOutline);
+                        if (tankGraphic[y, x + 1] == 0)
+                            bmp.SetPixel(x + 1, y, tankOutline);
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+        public abstract int GetArmour();
+
+        public abstract string[] WeaponList();
+
+        public abstract void FireWeapon(int weapon, ControlledTank playerTank, Gameplay currentGame);
+
+        public static TankModel GetTank(int tankNumber)
+        {
+            return new TheTank();
+        }
+    }
+    //create 4 
+    public class TheTank : TankModel
+    {
+        private int X1, Y1, X2, Y2;
+        private int currentDur;
+
+        public override int[,] DisplayTank(float angle)
+        {
+            int[,] graphic = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                   { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+                   { 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0 },
+                   { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
+
+            X1 = 7;
+            Y1 = 6;
+            X2 = (int)Math.Sin(angle);
+            Y2 = (int)Math.Cos(angle);
+            DrawLine(graphic, X1, Y1, X2, Y2);
+            return graphic;
+        }
+
+        public override void FireWeapon(int weapon, ControlledTank playerTank, Gameplay currentGame)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetArmour()
+        {
+            currentDur = 100;
+            return currentDur;
+        }
+
+        public override string[] WeaponList()
+        {
+            String[] Weapons = new string[] 
+            {
+                "Standard shell", "Medium Shell", "Large Shell",
+            };
+            return Weapons;
+        }
+    }
+}
