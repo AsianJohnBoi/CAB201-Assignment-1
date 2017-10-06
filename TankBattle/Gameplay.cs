1e9fd197408ccc01cc0f91a508ba87b33d8a05bd
AsianJohnBoi <Jbssantias@hotmail.com>
2017-10-07 00:10:56 +1000

    again

--- TankBattle/Gameplay.cs
+++ TankBattle/Gameplay.cs
@@ -8,155 +8,162 @@ using System.Windows.Forms;
 
 namespace TankBattle
 {
-    public class Gameplay
-    {
-        private int numPlayers;
-        private int numRounds;
+	public class Gameplay
+	{
+		private int[] numPlayers;
+		private int[] numRounds;
+		private int playerNum;
+		private List<string> WeaponsEffect;
+		private int currentRound;
+		private int opponent;
 
-        public Gameplay(int numPlayers, int numRounds)
-        {
-            this.numPlayers = numPlayers;
-            this.numRounds = numRounds;
-            List<string> WeaponsEffect = new List<string>();
-        }
 
-        public int PlayerCount()
-        {
-            return numPlayers;
-        }
+		public Gameplay(int numPlayers, int numRounds)
+		{
+			this.numPlayers = new int[numPlayers];
+			this.numRounds = new int[numRounds];
+			WeaponsEffect = new List<string>();
+		}
 
-        public int GetRound()
-        {
-            throw new NotImplementedException();
-        }
+		public int PlayerCount()
+		{
+			return numPlayers.Length;
+		}
 
-        public int GetTotalRounds()
-        {
-            return numRounds;
-        }
+		public int GetRound()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void SetPlayer(int playerNum, Opponent player)
-        {
-            throw new NotImplementedException();
-        }
+		public int GetTotalRounds()
+		{
+			return numRounds.Length;
+		}
 
-        public Opponent GetPlayerNumber(int playerNum)
-        {
-            throw new NotImplementedException();
-        }
+		public void SetPlayer(int playerNum, Opponent player)
+		{
+			playerNum = numPlayers.Length - 1;
+		}
 
-        public ControlledTank PlayerTank(int playerNum)
-        {
-            throw new NotImplementedException();
-        }
+		public Opponent GetPlayerNumber(int playerNum)
+		{
+			TankModel tank = TankModel.GetTank(1);
+			Opponent[] TheOppo = new Opponent[]
+			{
+			};
+			return TheOppo[playerNum];
+		}
 
-        public static Color GetTankColour(int playerNum)
-        {
-            Color[] TheColor = new Color[] { Color.AliceBlue, Color.Beige, Color.Chocolate, Color.Gold, Color.Orange, Color.Violet, Color.Yellow, Color.Green };
-            return TheColor[playerNum - 1];
-        }
+		public ControlledTank PlayerTank(int playerNum)
+		{
+			throw new NotImplementedException();
+		}
 
-        public static int[] CalculatePlayerPositions(int numPlayers)
-        {
-            throw new NotImplementedException();
-        }
+		public static Color GetTankColour(int playerNum)
+		{
+			Color[] TheColor = new Color[] { Color.AliceBlue, Color.Beige, Color.Chocolate, Color.Gold, Color.Orange, Color.Violet, Color.Yellow, Color.Green };
+			return TheColor[playerNum - 1];
+		}
 
-        public static void Shuffle(int[] array)
-        {
-            int[] shuffledArray = new int[array.Length];
-            int rndNo;
-            Random rnd = new Random();
-            for (int i = array.Length; i >= 1; i--)
-            {
-                rndNo = rnd.Next(1, i + 1) - 1;
-                shuffledArray[i - 1] = array[rndNo];
-                array[rndNo] = array[i - 1];
-            }
-            array = shuffledArray;
-        }
-        private int currentRound;
-        private int opponent;
+		public static int[] CalculatePlayerPositions(int numPlayers)
+		{
+			throw new NotImplementedException();
+		}
 
-        public void NewGame()
-        {
-            this.currentRound = 1;
-            this.opponent = 0;
-            BeginRound();
-        }
+		public static void Shuffle(int[] array)
+		{
+			int[] shuffledArray = new int[array.Length];
+			int rndNo;
+			Random rnd = new Random();
+			for (int i = array.Length; i >= 1; i--)
+			{
+				rndNo = rnd.Next(1, i + 1) - 1;
+				shuffledArray[i - 1] = array[rndNo];
+				array[rndNo] = array[i - 1];
+			}
+			array = shuffledArray;
+		}
+		public void NewGame()
+		{
+			this.currentRound = 1;
+			this.opponent = 0;
+			BeginRound();
+		}
 
-        public void BeginRound()
-        {
-            
-        }
+		public void BeginRound()
+		{
 
-        public Terrain GetLevel()
-        {
-            throw new NotImplementedException();
-        }
+		}
 
-        public void DrawPlayers(Graphics graphics, Size displaySize)
-        {
-            throw new NotImplementedException();
-        }
+		public Terrain GetLevel()
+		{
+			Terrain newRound = new Terrain();
+			return newRound;
+		}
 
-        public ControlledTank GetCurrentGameplayTank()
-        {
-            throw new NotImplementedException();
-        }
+		public void DrawPlayers(Graphics graphics, Size displaySize)
+		{
+			throw new NotImplementedException();
+		}
 
-        public void AddWeaponEffect(WeaponEffect weaponEffect)
-        {
-            throw new NotImplementedException();
-        }
+		public ControlledTank GetCurrentGameplayTank()
+		{
+			throw new NotImplementedException();
+		}
 
-        public bool ProcessEffects()
-        {
-            throw new NotImplementedException();
-        }
+		public void AddWeaponEffect(WeaponEffect weaponEffect)
+		{
+			throw new NotImplementedException();
+		}
 
-        public void RenderEffects(Graphics graphics, Size displaySize)
-        {
-            throw new NotImplementedException();
-        }
+		public bool ProcessEffects()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void EndEffect(WeaponEffect weaponEffect)
-        {
-            throw new NotImplementedException();
-        }
+		public void RenderEffects(Graphics graphics, Size displaySize)
+		{
+			throw new NotImplementedException();
+		}
 
-        public bool CheckHitTank(float projectileX, float projectileY)
-        {
-            throw new NotImplementedException();
-        }
+		public void EndEffect(WeaponEffect weaponEffect)
+		{
+			throw new NotImplementedException();
+		}
 
-        public void InflictDamage(float damageX, float damageY, float explosionDamage, float radius)
-        {
-            throw new NotImplementedException();
-        }
+		public bool CheckHitTank(float projectileX, float projectileY)
+		{
+			throw new NotImplementedException();
+		}
 
-        public bool Gravity()
-        {
-            throw new NotImplementedException();
-        }
+		public void InflictDamage(float damageX, float damageY, float explosionDamage, float radius)
+		{
+			throw new NotImplementedException();
+		}
 
-        public bool TurnOver()
-        {
-            throw new NotImplementedException();
-        }
+		public bool Gravity()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void RewardWinner()
-        {
-            throw new NotImplementedException();
-        }
+		public bool TurnOver()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void NextRound()
-        {
-            throw new NotImplementedException();
-        }
-        
-        public int GetWindSpeed()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void RewardWinner()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void NextRound()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetWindSpeed()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- TankBattle/HumanOpponent.cs
+++ TankBattle/HumanOpponent.cs
@@ -7,33 +7,35 @@ using System.Drawing;
 
 namespace TankBattle
 {
-    public class HumanOpponent : Opponent
-    {
+	public class HumanOpponent : Opponent
+	{
 		private string name;
 		private TankModel tank;
 		private Color colour;
 
 
-        public HumanOpponent(string name, TankModel tank, Color colour) : base(name, tank, colour)
-        {
+		public HumanOpponent(string name, TankModel tank, Color colour) : base(name, tank, colour)
+		{
 			this.name = name;
 			this.tank = tank;
 			this.colour = colour;
-        }
+		}
 
-        public override void StartRound()
-        {
-            throw new NotImplementedException();
-        }
+		public override void StartRound()
+		{
 
-        public override void BeginTurn(GameplayForm gameplayForm, Gameplay currentGame)
-        {
-            throw new NotImplementedException();
-        }
+		}
 
-        public override void ProjectileHitPos(float x, float y)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public override void BeginTurn(GameplayForm gameplayForm, Gameplay currentGame)
+		{
+			throw new NotImplementedException();
+			//finish GameplayForm
+		}
+
+		public override void ProjectileHitPos(float x, float y)
+		{
+			throw new NotImplementedException();
+			//finish GameplayForm
+		}
+	}
 }
