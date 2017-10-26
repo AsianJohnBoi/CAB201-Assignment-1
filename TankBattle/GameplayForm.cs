dfc5c0c25217eb100ad6dfbc947ec4e87bea134d
xikaz <xikazuchi@gmail.com>
2017-10-26 14:56:56 +1000

    Update GameplayForm.cs

--- TankBattle/GameplayForm.cs
+++ TankBattle/GameplayForm.cs
@@ -54,7 +54,7 @@ namespace TankBattle {
 
             //Set random background image and colour 
             backgroundImage = Image.FromFile(imageFilenames[randNumber]);
-            landscapeColour = Color.FromName(landscapeColours[randNumber].ToString());
+            landscapeColour = landscapeColour = (landscapeColours[randNumber]);
 
             InitializeComponent();
 
