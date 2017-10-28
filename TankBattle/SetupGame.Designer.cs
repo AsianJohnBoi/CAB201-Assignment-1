240ce9eed62c9f5835f00d90d39c3d1caf054e28
AsianJohnBoi <Jbssantias@hotmail.com>
2017-10-28 13:32:10 +1000

    added forms

---
+++ TankBattle/SetupGame.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace TankBattle
+{
+	partial class SetupGame
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Silver;
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.groupBox2);
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(415, 200);
+			this.panel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.label1.Location = new System.Drawing.Point(28, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(168, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Player #1\'s name:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.textBox1.Location = new System.Drawing.Point(193, 26);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(188, 30);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "Player 1";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Location = new System.Drawing.Point(33, 81);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(111, 67);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Controller";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(6, 29);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(59, 17);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Human";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(71, 29);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(35, 17);
+			this.radioButton2.TabIndex = 1;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "AI";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.radioButton3);
+			this.groupBox2.Location = new System.Drawing.Point(160, 81);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(221, 67);
+			this.groupBox2.TabIndex = 3;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Tank";
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(12, 31);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(14, 13);
+			this.radioButton3.TabIndex = 0;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(34, 158);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(347, 27);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Next Player";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// SetupGame
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Maroon;
+			this.ClientSize = new System.Drawing.Size(439, 224);
+			this.Controls.Add(this.panel1);
+			this.Name = "SetupGame";
+			this.Text = "SetUp Game";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ TankBattle/SetupPlayer.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TankBattle
+{
+	partial class SetupPlayer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+			this.domainUpDown2 = new System.Windows.Forms.DomainUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.label1.Location = new System.Drawing.Point(12, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(231, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "How many players? (2-8)";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+			this.label2.Location = new System.Drawing.Point(12, 64);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(270, 25);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "How many gameplay rounds?";
+			// 
+			// domainUpDown1
+			// 
+			this.domainUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+			this.domainUpDown1.Location = new System.Drawing.Point(301, 22);
+			this.domainUpDown1.Name = "domainUpDown1";
+			this.domainUpDown1.Size = new System.Drawing.Size(70, 27);
+			this.domainUpDown1.TabIndex = 2;
+			this.domainUpDown1.Text = "2";
+			// 
+			// domainUpDown2
+			// 
+			this.domainUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+			this.domainUpDown2.Location = new System.Drawing.Point(300, 64);
+			this.domainUpDown2.Name = "domainUpDown2";
+			this.domainUpDown2.Size = new System.Drawing.Size(70, 29);
+			this.domainUpDown2.TabIndex = 3;
+			this.domainUpDown2.Text = "1";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.button1.Location = new System.Drawing.Point(17, 105);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(353, 35);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Setup Players";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// SetupPlayer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(383, 152);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.domainUpDown2);
+			this.Controls.Add(this.domainUpDown1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "SetupPlayer";
+			this.Text = "Setup Player #1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DomainUpDown domainUpDown1;
+		private System.Windows.Forms.DomainUpDown domainUpDown2;
+		private System.Windows.Forms.Button button1;
+	}
+}
