f93dd1008352e840a3a533024f8c0e8e97a5eeb5
AsianJohnBoi <Jbssantias@hotmail.com>
2017-10-29 15:02:47 +1000

    forms done

--- TankBattle/SetupGame.cs
+++ TankBattle/SetupGame.cs
@@ -12,26 +12,18 @@ namespace TankBattle
 {
 	public partial class setupGameForm : Form
 	{
+		private int playerAmount, roundAmount;
+
 		public setupGameForm()
 		{
 			InitializeComponent();
 		}
 
-        private int playerAmount, roundAmount;
-
         private void setupPlayersButton_Click(object sender, EventArgs e) {
             playerAmount = (int)playerAmountInput.Value;
             roundAmount = (int)roundAmountInput.Value;
-
-            Gameplay game = new Gameplay(playerAmount, roundAmount);
-
-			for (int i = 0; i < playerAmount; i++)
-			{
-				Opponent player = new HumanOpponent("Player" + (i + 1), TankModel.GetTank(i+1), Gameplay.GetTankColour(i+1));
-				game.SetPlayer(i+1, player);
-			}
-            game.NewGame();
-
-        }
-    }
+			setupPlayerForm setupPlayer = new setupPlayerForm(playerAmount, roundAmount);
+			setupPlayer.Show();
+		}
+	}
 }
--- TankBattle/SetupPlayer.cs
+++ TankBattle/SetupPlayer.cs
@@ -12,9 +12,44 @@ namespace TankBattle
 {
 	public partial class setupPlayerForm : Form
 	{
-		public setupPlayerForm()
+		private int playerAmount, roundAmount;
+		private int player = 0;
+		private List<string> playerNames = new List<string>();
+
+		private void label1_Click(object sender, EventArgs e)
 		{
+
+		}
+
+		public setupPlayerForm(int playerAmount, int roundAmount)
+		{
+			this.playerAmount = playerAmount;
+			this.roundAmount = roundAmount;
 			InitializeComponent();
+		}
+
+		private void nextPlayerButton_Click(object sender, EventArgs e)
+		{
+			if (player < playerAmount)
+			{
+				PlayerLabel.Text = "Player #" + (player + 1) + "'s name:";
+				playerNames.Add(playerNameInput.Text);
+				playerNameInput.Clear();
+				player++;
+			}
+			else
+			{
+				Gameplay game = new Gameplay(playerAmount, roundAmount);
+
+				for (int i = 0; i < playerAmount; i++)
+				{
+					Opponent player = new HumanOpponent(playerNames[i], TankModel.GetTank(i + 1), Gameplay.GetTankColour(i + 1));
+					game.SetPlayer(i + 1, player);
+				}
+				game.NewGame();
+				Close();
+			}
+			
 		}
 	}
 }
