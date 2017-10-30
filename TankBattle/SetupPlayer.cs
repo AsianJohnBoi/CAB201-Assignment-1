e200f5bf90f1fede860198ced8c2a05bfa4dafd5
xikaz <xikazuchi@gmail.com>
2017-10-30 15:12:08 +1000

    Added XML comments to SetupPlayer

--- TankBattle/SetupPlayer.cs
+++ TankBattle/SetupPlayer.cs
@@ -1,14 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace TankBattle
 {
+    /// <summary>
+    /// 
+    /// This is the player setup form. The player's can set their names
+    /// controller and tank types.
+    /// 
+    /// Author John Santias and Hoang Nguyen October 2017
+    /// 
+    /// </summary>
 	public partial class setupPlayerForm : Form
 	{
 		private int playerAmount, roundAmount, player = 1;
 		private List<string> playerNames = new List<string>();
 
+        /// <summary>
+        /// 
+        /// This constructor takes in the player amount and round amount
+        /// specified in the setup game form.
+        /// 
+        /// Author John Santias and Hoang Nguyen October 2017
+        ///     
+        /// </summary>
+        /// <param name="playerAmount"> Amount of players </param>
+        /// <param name="roundAmount"> Amount of rounds </param>
 		public setupPlayerForm(int playerAmount, int roundAmount)
 		{
 			this.playerAmount = playerAmount;
@@ -16,12 +34,32 @@ namespace TankBattle
 			InitializeComponent();
 		}
 
+        /// <summary>
+        /// 
+        /// When the form is loaded, the BackColor of the form is changed
+        /// accordingly to their assigned tank colour.
+        /// 
+        /// Author John Santias and Hoang Nguyen October 2017
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void setupPlayerForm_Load(object sender, EventArgs e)
 		{
 			BackColor = Gameplay.GetTankColour(player);
 			nextPlayerButton.Enabled = false;
 		}
 
+        /// <summary>
+        /// 
+        /// The next player button is disabled until a value in 
+        /// the player name text box is not null.
+        ///
+        /// Author John Santias and Hoang Nguyen October 2017
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void playerNameInput_TextChanged(object sender, EventArgs e)
 		{
 			if (String.IsNullOrEmpty(playerNameInput.Text))
@@ -32,6 +70,17 @@ namespace TankBattle
 			}
 		}
 
+        /// <summary>
+        /// 
+        /// When this button is pressed, the next player can select their
+        /// name, tank and controller. When all players have selected their loadouts
+        /// the game will begin.
+        /// 
+        /// Author John Santias and Hoang Nguyen October 2017
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void nextPlayerButton_Click(object sender, EventArgs e)
 		{
 			playerNames.Add(playerNameInput.Text);
